4ef5778f8cf1bcd6738bcbea0798a8462336d577
Chengyu Yao <cyao@teslamotors.com>
2015-10-08 14:47:19 -0700

    Tests NAND and XOR

--- Backend/System/NeuralNet/Trainer.py
+++ Backend/System/NeuralNet/Trainer.py
@@ -1,15 +1,19 @@
 import os
 import csv
+import textwrap
 from ArtificialNeuralNetwork import ArtificialNeuralNetwork
 
 
-DATA_PATH = "Data/XOR.csv"
+DATA_PATH = "Test/XOR.csv"
 CYCLE = 5000
 
 
 def train(topology, inputValues, targetValues):
     net = ArtificialNeuralNetwork(topology)
     count = 0
+    threshold = None
+    # When we expect the neural network to learn at earliest
+    fastSuccess = 500
     while count < CYCLE:
         index = count % len(listInputValues)
         # propagate forward
@@ -23,8 +27,18 @@ def train(topology, inputValues, targetValues):
         net.back_prop(listTargetValues[index])
         print ("Recent Average Error: {}".format(net.recentAverageError))
         print ("---------------\n")
+        # Find when it reached 95%+ accuracy
+        if net.recentAverageError < 0.05 and not threshold:
+            if count > fastSuccess:
+                threshold = count
         count += 1
+    
+    print (textwrap.dedent("""\
+        Stats:
+            
+            Reached 95% accuracy at: {}
         
+    """.format(threshold)))
         
 if __name__ == "__main__":
     topology = []
